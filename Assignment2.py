18ec35f462ce3abd9385a12f62dd0137f21e863c
Andrew Chan <mc220517312@student.unitar.my>
2023-03-05 15:05:25 +0800

    Complete

--- Assignment2.py
+++ Assignment2.py
@@ -1,56 +1,66 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
-# load the dataset
-sales_df = pd.read_csv('sales_data_na.csv')
+# Load the data
+sales_data_url = "https://raw.githubusercontent.com/mc220517312/Assignment-2/main/sales_data_na.csv"
+sales_data = pd.read_csv(sales_data_url, sep=',')
 
-# check the size of the dataset
-print("Number of rows:", sales_df.shape[0])
-print("Number of columns:", sales_df.shape[1])
+# Clean the data
+sales_data.columns = sales_data.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('-', '_')
+sales_data = sales_data.drop_duplicates()
+sales_data = sales_data.dropna()
 
-# check for missing data
-print(sales_df.isnull().sum())
+# Most Profitable Year
+profit_by_year = sales_data.groupby('year')['profit'].sum()
+profit_by_year.plot(kind='bar')
+plt.xlabel('Year')
+plt.ylabel('Profit')
+plt.title('Profit by Year')
+plt.show()
 
-# check for duplicates
-print("Number of duplicate rows:", sales_df.duplicated().sum())
+# Most Profitable Month
+profit_by_month = sales_data.groupby('month')['profit'].sum()
+profit_by_month.plot(kind='bar')
+plt.xlabel('Month')
+plt.ylabel('Profit')
+plt.title('Profit by Month')
+plt.show()
 
-# check for outliers
-sns.boxplot(x=sales_df['Profit'])
+# Gender Having Most Order
+order_by_gender = sales_data.groupby('customer_gender')['order_quantity'].sum()
+order_by_gender.plot(kind='bar')
+plt.xlabel('Gender')
+plt.ylabel('Order Quantity')
+plt.title('Order Quantity by Gender')
+plt.show()
 
-# drop duplicate rows
-sales_df.drop_duplicates(inplace=True)
+# Correlation between Annual Sales
+annual_sales = sales_data.groupby('year')['revenue'].sum()
+annual_sales.plot(kind='line', marker='o')
+plt.xlabel('Year')
+plt.ylabel('Sales')
+plt.title('Annual Sales')
+plt.show()
 
-# rename columns to make them consistent
-sales_df.rename(columns={'Product_Category': 'Category', 'Sub_Category': 'Subcategory', 'Unit_Cost': 'Cost', 'Unit_Price': 'Price'}, inplace=True)
+# Category Generates Most Profit
+profit_by_category = sales_data.groupby('product_category')['profit'].sum()
+profit_by_category.plot(kind='bar')
+plt.xlabel('Category')
+plt.ylabel('Profit')
+plt.title('Profit by Category')
+plt.show()
 
-# Replace 'Aogust' with 'August'
-sales_df['Month'] = sales_df['Month'].str.replace('Aogust', 'August')
+# Sub-Category Generates Most Profit
+profit_by_subcategory = sales_data.groupby('sub_category')['profit'].sum()
+profit_by_subcategory.plot(kind='bar')
+plt.xlabel('Sub-Category')
+plt.ylabel('Profit')
+plt.title('Profit by Sub-Category')
+plt.show()
 
-# Replace 'Marsh' with 'March'
-sales_df['Month'] = sales_df['Month'].str.replace('Marsh', 'March')
-
-# Replace 'Jone' with 'June'
-sales_df['Month'] = sales_df['Month'].str.replace('Jone', 'June')
-
-# Replace 'Joly' with 'July'
-sales_df['Month'] = sales_df['Month'].str.replace('Joly', 'July')
-
-# tidy the dataset by converting month names to numbers
-sales_df['Month'] = pd.to_datetime(sales_df['Month'], format='%B').dt.month
-
-# convert data types to correct format
-sales_df['Order_Quantity'] = sales_df['Order_Quantity'].astype('int64')
-sales_df['Cost'] = sales_df['Cost'].astype('float64')
-sales_df['Price'] = sales_df['Price'].astype('float64')
-sales_df['Profit'] = sales_df['Profit'].astype('float64')
-sales_df['Revenue'] = sales_df['Revenue'].astype('float64')
-
-# plot the relationship between profit and revenue
-sns.scatterplot(x=sales_df['Profit'], y=sales_df['Revenue'])
-
-# plot the distribution of ages
-sns.histplot(data=sales_df, x='Customer_Age', bins=10)
-
-# plot the number of sales per category
-sns.countplot(data=sales_df, x='Category')
+# Sales per Country
+sales_by_country = sales_data.groupby('country')['revenue'].sum()
+sales_by_country.plot(kind='pie', autopct='%1.1f%%')
+plt.title('Sales by Country')
+plt.show()
